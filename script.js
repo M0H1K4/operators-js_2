49018be582ad6a418b995967c2bd75b8fa146647
M0H1K4 <nejer911@gmail.com>
2024-10-30 20:57:41 +0400

    funqcia mivaniche

---
+++ script.js
@@ -0,0 +1,75 @@
+
+// არითმეტიკული ოპერატორები
+// let a = 10;
+// let b = 2;
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+///////////////////////////////
+
+
+// მინიჭების ოპერატორები
+
+// let x = 5;
+// let y = 10;
+
+// console.log(x+=y)
+// console.log(x-=y)
+// console.log(x*=y)
+// console.log(x/=y)
+// console.log(x%=y)
+
+
+
+// შედარების ოპერატორები
+// console.log(10 == '10')
+// console.log(10 === '10')
+// console.log(10 != 5)
+// console.log(10 > 5)
+// console.log(10 < 5)
+// console.log(10 <= 5)
+// console.log(10 >= 5)
+
+// ლოგიკური ოპერატორები
+// let a =  true;
+// let b = false;
+
+
+// console.log(a && b) // "და"  ორივე უნდა იყოს true
+// console.log(a || b) // "ან"  ერთი მაინც უნდა იყოს true
+// console.log(!a) // თუ არის true გამოიტანს false....
+// console.log(!(a && b)) 
+
+
+
+// Template Literals
+
+// let name  = "Luka"
+// let secondName = "Kheladze"
+
+// console.log(`გამარჯობა მე მქვია ${name} , ჩემი გვარია ${secondName} `)
+
+////////////////////
+
+
+// პირობები
+
+
+// let temperature = 25;
+
+// if(temperature >= 40 ){
+//     console.log("გარეთ ძალიან ცხელა დარჩი სახლში!")
+// } else{
+//     console.log("გადი ისეირნე!")
+// }
+
+
+
+// accecing characters
+
+let str = "გამარჯობა"
+
+console.log(str[8])
